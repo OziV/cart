025a00415583debcd90be474d3c6a599235a7b21
OziV <oz.ziv8@gmail.com>
2022-08-16 18:10:17 +0300

    WIP: done

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useGlobalContext } from "./context";
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
 
 function Home() {
-  return <div>Home</div>
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 
 export default Home;
